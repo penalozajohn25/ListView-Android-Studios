3fca82dd91cf94d49348d74d6bf662923c088c2d
root <root@localhost.localdomain>
2020-02-24 15:22:04 -0400

    listview Android Studio

---
+++ app/src/main/java/developer/app/com/practicaandroid/MainActivity.java
@@ -0,0 +1,37 @@
+package developer.app.com.practicaandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private ListView lv1;
+
+    private String nombres [] = {"Juan", "Pedro", "Maria", "Julia"};
+    private String edades [] = {"12", "14", "28", "31"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.tv1);
+        lv1 = (ListView) findViewById(R.id.lv1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_practica, nombres);
+        lv1.setAdapter(adapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tv1.setText("la edad de " + lv1.getItemAtPosition(i) + "es: " + edades[i] + "años");
+            }
+        });
+    }
+}
